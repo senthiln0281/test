4b558b78ef96b87bc1c866ceb842c9adcc07c3ba
senthiln0281 <senthiln0281@gmail.com>
2018-06-22 14:17:34 +0000

    added nnewcode

---
+++ main.tf
@@ -0,0 +1,28 @@
+provider "aws"{
+  region = "${var.region}"
+}
+resource "aws_s3_bucket" "497704" {
+  bucket = "${var.bucket}"
+  acl = "${var.acl}"
+  policy = <<EOF
+{
+  "Id": "bucket_policy_site",
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "bucket_policy_site_main",
+      "Action": [
+        "s3:GetObject"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::${var.bucket}/*",
+      "Principal": "*"
+    }
+  ]
+}
+EOF
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "region"{
+  default = "us-west-2"
+}
+variable "bucket" {
+  default = "wildrydes-senthil-497704"
+}
+variable "source" {
+  default = "/usr/bin/terratest"
+}
+variable "etag" {
+  default = "wildrydesimage"
+}
+variable "acl" {
+  default = "public-read"
+}
