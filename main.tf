91fb47b0c61314484334602b920baac00f99efa6
senthiln0281 <senthiln0281@gmail.com>
2018-07-03 08:05:56 +0000

    chcognitchange

--- main.tf
+++ main.tf
@@ -33,6 +33,6 @@ resource "aws_cognito_user_pool_client" "client" {
   name = "WildRydesWebApp"
 
   user_pool_id = "${aws_cognito_user_pool.pool.id}"
-  client_secret = false
+  generate_secret = false
   
 }
