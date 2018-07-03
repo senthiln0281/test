4f21e21f7ce73cf323c4359e7b81fd7791fffdee
senthiln0281 <senthiln0281@gmail.com>
2018-07-03 08:04:01 +0000

    chcognitchanges

--- main.tf
+++ main.tf
@@ -27,5 +27,12 @@ EOF
   }
 }
 resource "aws_cognito_user_pool" "pool" {
-  name = WildRydes"
+  name = "WildRydes"
+}
+resource "aws_cognito_user_pool_client" "client" {
+  name = "WildRydesWebApp"
+
+  user_pool_id = "${aws_cognito_user_pool.pool.id}"
+  client_secret = false
+  
 }
