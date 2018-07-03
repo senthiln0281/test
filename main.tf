5654e98e428e15ff2d72ce079fbd5a0d00d6ea9d
senthiln0281 <senthiln0281@gmail.com>
2018-07-03 07:32:51 +0000

    chcognito

--- main.tf
+++ main.tf
@@ -26,13 +26,3 @@ EOF
     error_document = "error.html"
   }
 }
-resource "aws_cognito_user_pool" "pool" {
-  name = WildRydes"
-}
-resource "aws_cognito_user_pool_client" "client" {
-  name = "WildRydesWebApp"
-
-  user_pool_id = "${aws_cognito_user_pool.pool.id}"
-  client_secret = false
-  
-}
