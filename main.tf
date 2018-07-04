4f268da5603f7034221062d006baf28ab3c364ea
senthiln0281 <senthiln0281@gmail.com>
2018-07-04 19:08:51 +0530

    Roleadd

--- main.tf
+++ main.tf
@@ -84,7 +84,7 @@ resource "aws_iam_role_policy_attachment" "exec-role" {
     policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 resource "aws_iam_role_policy_attachment" "exec-role1" {
-    role      = "${aws_iam_role1.iam_for_lambda.name}"
+    role      = "${aws_iam_role.iam_for_lambda.name}"
     policy_arn = "arn:aws:iam::aws:policy/service-role/DynamoDBWriteAccess"
 }
 
