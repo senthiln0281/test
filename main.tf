fb998cf38fbe0d73f298f9353f8cfbdc17d88862
senthiln0281 <senthiln0281@gmail.com>
2018-07-04 17:24:31 +0530

    IAMrolechan

--- main.tf
+++ main.tf
@@ -82,6 +82,7 @@ EOF
 
 resource "aws_iam_role_policy_attachment" "exec-role" {
     role       = "${aws_iam_role.iam_for_lambda.name}"
+    role       = "${aws_iam_role1.iam_for_lambda.name}
     policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
     policy_arn = "arn:aws:iam::aws:policy/service-role/DynamoDBWriteAccess"
 }
