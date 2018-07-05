246c5550f38f6a4ded1d5a43b4d0c653b590d7f3
senthiln0281 <senthiln0281@gmail.com>
2018-07-05 13:26:58 +0530

    adpolicy

--- main.tf
+++ main.tf
@@ -91,9 +91,10 @@ resource "aws_iam_policy" "policy" {
       "Effect": "Allow",
       "Resource": "arn:aws:lambda:us-west-2:033219852540:function:RequestUnicorn"
     }
+}
   ]
 }
-}
+
 resource "aws_iam_role_policy_attachment" "exec-role" {
     role      = "${aws_iam_role.iam_for_lambda.name}"
     policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
