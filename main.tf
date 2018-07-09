f95f17066092ead6ec2816dcd3e19994dff3b269
senthiln0281 <senthiln0281@gmail.com>
2018-07-09 14:44:34 +0530

    lambdaw

--- main.tf
+++ main.tf
@@ -88,6 +88,14 @@ resource "aws_iam_role_policy_attachment" "exec-role1" {
     role      = "${var.aws_iam_role}"
     policy_arn = "${aws_iam_policy.policy.arn}"
 }
+resource "aws_lambda_function" "RequestUnicorn1" {
+  function_name    = "requestunicorn1"
+  role             = "${aws_iam_role.iam_for_lambda.arn}"
+  handler          = "requestunicorn.lambda_handler"
+  runtime          = "python2.7"
+  s3_bucket = "wildrydes-senthil-497704"
+  s3_key = "requestunicorn.zip"
+}
 
 resource "aws_s3_bucket_object" "object" {
   bucket = "wildrydes-senthil-497704"
@@ -121,14 +129,7 @@ resource "aws_iam_policy" "policy" {
 EOF
 }
 
-resource "aws_lambda_function" "RequestUnicorn1" {
-  function_name    = "requestunicorn1"
-  role             = "${aws_iam_role.iam_for_lambda.arn}"
-  handler          = "requestunicorn.lambda_handler"
-  runtime          = "python2.7"
-  s3_bucket = "wildrydes-senthil-497704"
-  s3_key = "requestunicorn.zip"
-}
+
 data "terraform_remote_state" "foo" {
   backend = "local"
 
