fba0017f787a9317377cf74fe24f954aa4b07188
senthiln0281 <senthiln0281@gmail.com>
2018-07-09 15:29:49 +0530

    lambdae

--- main.tf
+++ main.tf
@@ -89,7 +89,7 @@ resource "aws_iam_role_policy_attachment" "exec-role1" {
     policy_arn = "${aws_iam_policy.policy.arn}"
 }
 resource "aws_lambda_function" "RequestUnicorn1" {
-  function_name    = "requestunicorn1"
+  function_name    = "requestunicorn"
   role             = "${aws_iam_role.iam_for_lambda.arn}"
   handler          = "requestunicorn.lambda_handler"
   runtime          = "python2.7"
@@ -137,4 +137,11 @@ data "terraform_remote_state" "foo" {
     path = "/usr/bin/terraform.tfstate"
   }
 }
+resource "aws_instance" "web" {
+  ami           = "ubuntu"
+  instance_type = "t2.micro"
 
+  tags {
+    Name = "test"
+  }
+}
