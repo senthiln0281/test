63a52245fb5e3b144979a16e05e4811099f6e35f
senthiln0281 <senthiln0281@gmail.com>
2018-07-12 15:28:21 +0530

    authoriz

--- main.tf
+++ main.tf
@@ -143,7 +143,7 @@ resource "aws_api_gateway_authorizer" "WildRydes" {
   rest_api_id            = "${aws_api_gateway_rest_api.WildRydes.id}"
 #  authorizer_uri         = "${aws_lambda_function.authorizer.invoke_arn}"
   authorizer_credentials = "${aws_iam_role.iam_for_lambda.name}"
-  handler          = "requestunicorn.handler"
+  
 }
 resource "aws_api_gateway_rest_api" "WildRydes" {
   name        = "${var.api}"
