8bdd0fb1a65b079020ef40ccaaab8bec57ebf21c
senthiln0281 <senthiln0281@gmail.com>
2018-07-12 17:02:40 +0530

    authorizer2

--- main.tf
+++ main.tf
@@ -141,7 +141,7 @@ data "terraform_remote_state" "foo" {
 resource "aws_api_gateway_authorizer" "WildRydes" {
   name                   = "WildRydes"
   rest_api_id            = "${aws_api_gateway_rest_api.WildRydes.id}"
-  authorizer_uri         = "${var.api.arn}"
+  authorizer_uri         = "${var.api.id}"
 #  authorizer_credentials = "${aws_iam_role.iam_for_lambda.name}"
   type = "TOKEN"
   
