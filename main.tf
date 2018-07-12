a61ec228f0d8fb48dcf15ff16447c45fc89db3f3
senthiln0281 <senthiln0281@gmail.com>
2018-07-12 15:09:36 +0530

    autho

--- main.tf
+++ main.tf
@@ -151,5 +151,5 @@ resource "aws_api_gateway_authorizer" "demo" {
   name                   = "WildRydes"
   rest_api_id            = "${aws_api_gateway_rest_api.WildRydes.id}"
 #  authorizer_uri         = "${aws_lambda_function.authorizer.invoke_arn}"
-  authorizer_credentials = "${aws_iam_role.invocation_role.arn}"
+  authorizer_credentials = "${aws_iam_role.iam_for_lambda.name}"
 }
