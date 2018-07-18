d24391c9505166cc4fde2e1411bfebba032ddb9e
senthiln0281 <senthiln0281@gmail.com>
2018-07-18 19:37:27 +0530

    apires

--- main.tf
+++ main.tf
@@ -154,4 +154,15 @@ resource "aws_api_gateway_rest_api" "WildRydes" {
   }
 }
 
+resource "aws_api_gateway_resource" "ride" {
+  rest_api_id = "${aws_api_gateway_rest_api.WildRydes.id}"
+  parent_id   = "${aws_api_gateway_rest_api.WildRydes.root_resource_id}"
+  path_part   = "mydemoresource"
+}
 
+resource "aws_api_gateway_method" "POST" {
+  rest_api_id   = "${aws_api_gateway_rest_api.WildRydes.id}"
+  resource_id   = "${aws_api_gateway_resource.WildRydes.id}"
+  http_method   = "POST"
+  authorization = "NONE"
+}
