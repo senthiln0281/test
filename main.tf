ea7e3b749198d3b52d9dc2511f07476ba76ae6ef
senthiln0281 <senthiln0281@gmail.com>
2018-07-19 11:51:43 +0530

    rmch

--- main.tf
+++ main.tf
@@ -163,8 +163,9 @@ resource "aws_api_gateway_resource" "ride" {
 resource "aws_api_gateway_method" "POST" {
   rest_api_id   = "${aws_api_gateway_rest_api.WildRydes.id}"
   resource_id   = "${aws_api_gateway_resource.ride.id}"
-  http_method   = "POST"
+#  http_method   = "POST"
   selection_pattern = "${aws_lambda_function.RequestUnicorn.arn}"
+  integration_http_method = "POST"
   authorization = "NONE"
 }
 
