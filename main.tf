e2a7b0728972bcdca687ab3c33deb4acadb3dd13
senthiln0281 <senthiln0281@gmail.com>
2018-07-19 14:49:48 +0530

    apideploy

--- main.tf
+++ main.tf
@@ -203,9 +203,9 @@ resource "aws_api_gateway_integration_response" "WildRydes" {
   http_method = "${aws_api_gateway_method.POST.http_method}"
   status_code = "${aws_api_gateway_method_response.200.status_code}"
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = true,
-    "method.response.header.Access-Control-Allow-Methods" = true,
-    "method.response.header.Access-Control-Allow-Origin" = true
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS,GET,PUT,PATCH,DELETE'",
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
   }
 }
 
