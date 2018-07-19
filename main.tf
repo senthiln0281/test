8c9427ba3dc212d19d7bd9403affc1e066f3ca86
senthiln0281 <senthiln0281@gmail.com>
2018-07-19 14:57:34 +0530

    apideploy

--- main.tf
+++ main.tf
@@ -176,6 +176,7 @@ resource "aws_api_gateway_integration" "Integration" {
 #  selection_pattern = "${aws_api_gateway_method.POST.selection_pattern}"
   http_method = "${aws_api_gateway_method.POST.http_method}"
   type        = "MOCK"
+
 }
 
 
@@ -194,19 +195,20 @@ resource "aws_api_gateway_method_response" "200" {
   resource_id = "${aws_api_gateway_resource.ride.id}"
   http_method = "${aws_api_gateway_method.POST.http_method}"
   status_code = "200"
-
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
+  depends_on = ["aws_api_gateway_method_response.200"]
 }
+
 
 resource "aws_api_gateway_integration_response" "WildRydes" {
   rest_api_id = "${aws_api_gateway_rest_api.WildRydes.id}"
   resource_id = "${aws_api_gateway_resource.ride.id}"
   http_method = "${aws_api_gateway_method.POST.http_method}"
   status_code = "${aws_api_gateway_method_response.200.status_code}"
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
-    "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS,GET,PUT,PATCH,DELETE'",
-    "method.response.header.Access-Control-Allow-Origin" = "'*'"
-  }
 }
 
 resource "aws_api_gateway_deployment" "Deployment" {
