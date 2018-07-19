f6c3db68c14badcb2d06f08bba5d8b38ee0b8b72
senthiln0281 <senthiln0281@gmail.com>
2018-07-19 11:56:03 +0530

    rmch

--- main.tf
+++ main.tf
@@ -172,7 +172,8 @@ resource "aws_api_gateway_method" "POST" {
 resource "aws_api_gateway_integration" "MyDemoIntegration" {
   rest_api_id = "${aws_api_gateway_rest_api.WildRydes.id}"
   resource_id = "${aws_api_gateway_resource.ride.id}"
-  selection_pattern = "${aws_api_gateway_method.POST.selection_pattern}"
+#  selection_pattern = "${aws_api_gateway_method.POST.selection_pattern}"
+  http_method = "${aws_api_gateway_method.POST.http_method}"
   type        = "MOCK"
 }
 
