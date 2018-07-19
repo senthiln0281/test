ca6f8af4e6ed168e241fbf14b229c8c0f3ecc6a4
senthiln0281 <senthiln0281@gmail.com>
2018-07-19 15:23:31 +0530

    apideploym

--- main.tf
+++ main.tf
@@ -195,6 +195,21 @@ resource "aws_api_gateway_method_response" "200" {
   resource_id = "${aws_api_gateway_resource.ride.id}"
   http_method = "${aws_api_gateway_method.POST.http_method}"
   status_code = "200"
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+
+}
+
+
+resource "aws_api_gateway_integration_response" "WildRydes" {
+  rest_api_id = "${aws_api_gateway_rest_api.WildRydes.id}"
+  resource_id = "${aws_api_gateway_resource.ride.id}"
+  http_method = "${aws_api_gateway_method.POST.http_method}"
+  status_code = "${aws_api_gateway_method_response.200.status_code}"
+
   response_models = {
     "application/json" = "Empty"
   }
@@ -204,14 +219,6 @@ resource "aws_api_gateway_method_response" "200" {
     "method.response.header.Access-Control-Allow-Origin" = "'*'"
   }
   depends_on = ["aws_api_gateway_method_response.200"]
-}
-
-
-resource "aws_api_gateway_integration_response" "WildRydes" {
-  rest_api_id = "${aws_api_gateway_rest_api.WildRydes.id}"
-  resource_id = "${aws_api_gateway_resource.ride.id}"
-  http_method = "${aws_api_gateway_method.POST.http_method}"
-  status_code = "${aws_api_gateway_method_response.200.status_code}"
 
 }
 
