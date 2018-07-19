c52c0e2745a052d7e2bdb2dcdf21129b29d6db7e
senthiln0281 <senthiln0281@gmail.com>
2018-07-19 16:32:08 +0530

    apideploym

--- main.tf
+++ main.tf
@@ -174,7 +174,7 @@ resource "aws_api_gateway_integration" "Integration" {
   rest_api_id = "${aws_api_gateway_rest_api.WildRydes.id}"
   resource_id = "${aws_api_gateway_resource.ride.id}"
 #  selection_pattern = "${aws_api_gateway_method.POST.selection_pattern}"
-  http_method = "${aws_api_gateway_method.POST.http_method}"
+#  http_method = "${aws_api_gateway_method.POST.http_method}"
   type        = "AWS_PROXY"
 
 }
