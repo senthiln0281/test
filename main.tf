41e32d1de99b30a5f652568188d60fb9fbbc9502
senthiln0281 <senthiln0281@gmail.com>
2018-07-20 10:51:28 +0530

    apide

--- main.tf
+++ main.tf
@@ -188,7 +188,7 @@ resource "aws_lambda_permission" "apigw_lambda" {
   principal     = "apigateway.amazonaws.com"
 
   # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
-  source_arn = "arn:aws:execute-api:${va.region}:${var.accountId}:${aws_api_gateway_rest_api.WildRydes.id}/*/${aws_api_gateway_method.POST.http_method}${aws_api_gateway_resource.ride.path}"
+  source_arn = "arn:aws:execute-api:${var.region}:${var.accountId}:${aws_api_gateway_rest_api.WildRydes.id}/*/${aws_api_gateway_method.POST.http_method}${aws_api_gateway_resource.ride.path}"
 }
 
 resource "aws_api_gateway_method_response" "200" {
