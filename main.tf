2401abc9efc7b7ee7984923784b2be6c0f463e71
senthiln0281 <senthiln0281@gmail.com>
2018-07-20 14:53:26 +0530

    apidep

--- main.tf
+++ main.tf
@@ -211,11 +211,10 @@ resource "aws_api_gateway_integration_response" "WildRydes" {
   http_method = "${aws_api_gateway_method.POST.http_method}"
   status_code = "${aws_api_gateway_method_response.200.status_code}"
 
-/*  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
-    "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,POST'",
+  response_parameters = {
+
     "method.response.header.Access-Control-Allow-Origin" = "'*'"
-  } */
+  }
   depends_on = ["aws_api_gateway_method_response.200"]
 
 }
