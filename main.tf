58775cc09501449e8487875280815ec717d057e9
senthiln0281 <senthiln0281@gmail.com>
2018-07-20 12:48:59 +0530

    apide

--- main.tf
+++ main.tf
@@ -216,7 +216,7 @@ resource "aws_api_gateway_integration_response" "WildRydes" {
     "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
     "method.response.header.Access-Control-Allow-Origin" = "'*'"
   }
-  depends_on = ["aws_api_gateway_method_response.200"]
+#  depends_on = ["aws_api_gateway_method_response.200"]
 
 }
 
